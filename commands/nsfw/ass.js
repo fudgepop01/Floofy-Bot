59e549da9acc0e34126f41206d0d6c8898a46340
fudgepop01 <fudgepop01@gmail.com>
2017-03-03 15:48:07 -0500

    inserted deez checks
    and deez ||

--- commands/nsfw/ass.js
+++ commands/nsfw/ass.js
@@ -17,8 +17,8 @@ module.exports = class AssCommand extends Command {
 		let settings = await guildSettings.findOne({ where: { guildID: msg.guild.id } });
 		if (!settings) settings = await guildSettings.create({ guildID: msg.guild.id });
 		const nsfw = settings.nsfw;
-		if (!nsfw && !nsfw.enabled) return msg.channel.send('NSFW is disabled for this server.');
-		if (!nsfw.channels.includes(msg.channel.id)) return msg.channel.send('NSFW is disabled for this channel.');
+		if (!nsfw || !nsfw.enabled) return msg.channel.send('NSFW is disabled for this server.');
+		if (!nsfw.channels || !nsfw.channels.includes(msg.channel.id)) return msg.channel.send('NSFW is disabled for this channel.');
 		return superagent.get('http://api.obutts.ru/butts/0/1/random').then(res => {
 			msg.channel.send(`http://media.obutts.ru/${res[0].preview}`);
 		})
